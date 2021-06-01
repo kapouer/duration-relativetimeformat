a5b3f1bc8fe530b2194a32688e7976793ec6bc18
Jérémy Lal <kapouer@melix.org>
2021-06-01 10:28:05 +0200

    Make tests easier to read

--- test/unit.js
+++ test/unit.js
@@ -8,77 +8,153 @@ const Duration = require('..');
 
 const assert = require('assert');
 
+function check(start, end, str) {
+	assert.strictEqual(Duration('en')(new Date(start), new Date(end)), str);
+}
+
 describe("Unit tests", () => {
 	let NOW;
 	beforeEach(() => {
 		NOW = Math.floor(Date.now() / 1000) * 1000;
 	});
 	it('should be now', function () {
-		assert.equal(Duration('en')(new Date()), 'now');
+		check(
+			NOW,
+			NOW,
+			'now'
+		);
 	});
 
 	it('should be now when before/after 30 seconds', function () {
-		assert.equal(Duration('en')(new Date(NOW + 29 * Duration.second)), 'now');
-		assert.equal(Duration('en')(new Date(), new Date(NOW + 29 * Duration.second)), 'now');
+		check(
+			NOW + 29 * Duration.second,
+			NOW,
+			'now'
+		);
+		check(
+			NOW,
+			NOW + 29 * Duration.second,
+			'now'
+		);
 	});
 
 	it('should be in 31 seconds', function () {
-		assert.equal(Duration('en')(new Date(NOW + 31 * Duration.second), new Date(NOW)), 'in 31 seconds');
+		check(
+			NOW + 31 * Duration.second,
+			NOW,
+			'in 31 seconds'
+		);
 	});
 
 	it('should be 31 seconds ago', function () {
-		assert.equal(Duration('en')(new Date(NOW), new Date(NOW + 31 * Duration.second)), '31 seconds ago');
+		check(
+			NOW,
+			NOW + 31 * Duration.second,
+			'31 seconds ago'
+		);
 	});
 
 	it('should be in 1 minute', function () {
-		assert.equal(Duration('en')(new Date(NOW + Duration.minute), new Date(NOW)), 'in 1 minute');
+		check(
+			NOW + Duration.minute,
+			NOW,
+			'in 1 minute'
+		);
 	});
 
-	it('should be in 1 minute too', function () {
-		assert.equal(Duration('en')(new Date(NOW + Duration.minute + 31 * Duration.second)), 'in 1 minute');
+	it('should count 1mn29s as 1 minute', function () {
+		check(
+			NOW + Duration.minute + 29 * Duration.second,
+			NOW,
+			'in 1 minute'
+		);
 	});
 
 	it('should be 3 hours ago', function () {
-		assert.equal(Duration('en')(new Date(NOW), new Date(NOW + 3 * Duration.hour)), '3 hours ago');
+		check(
+			NOW,
+			NOW + 3 * Duration.hour,
+			'3 hours ago'
+		);
 	});
 
 	it('should be yesterday', function () {
-		assert.equal(Duration('en')(new Date(NOW - Duration.hour), new Date(NOW + Duration.day)), 'yesterday');
+		check(
+			NOW - Duration.hour,
+			NOW + Duration.day,
+			'yesterday'
+		);
 	});
 
 	it('should be yesterday too', function () {
-		assert.equal(Duration('en')(new Date(NOW - 23 * Duration.hour), new Date(NOW + Duration.day)), 'yesterday');
+		check(
+			NOW - 23 * Duration.hour,
+			NOW + Duration.day,
+			'yesterday'
+		);
 	});
 
 	it('should be 2 days ago', function () {
-		assert.equal(Duration('en')(new Date(NOW - 24 * Duration.hour), new Date(NOW + Duration.day)), '2 days ago');
+		check(
+			NOW - Duration.day,
+			NOW + Duration.day,
+			'2 days ago'
+		);
 	});
 
 	it('should be six days ago', function () {
-		assert.equal(Duration('en')(new Date(NOW), new Date(NOW + Duration.week - Duration.day)), '6 days ago');
+		check(
+			NOW,
+			NOW + Duration.week - Duration.day,
+			'6 days ago'
+		);
 	});
 
 	it('should be last week', function () {
-		assert.equal(Duration('en')(new Date(NOW), new Date(NOW + Duration.week)), 'last week');
+		check(
+			NOW,
+			NOW + Duration.week,
+			'last week'
+		);
 	});
 
 	it('should be last week too', function () {
-		assert.equal(Duration('en')(new Date(NOW), new Date(NOW + Duration.week + Duration.day)), 'last week');
+		check(
+			NOW,
+			NOW + Duration.week + Duration.day,
+			'last week'
+		);
 	});
 
 	it('should be 2 weeks ago', function () {
-		assert.equal(Duration('en')(new Date(NOW), new Date(NOW + 2 * Duration.week)), '2 weeks ago');
+		check(
+			NOW,
+			NOW + 2 * Duration.week,
+			'2 weeks ago'
+		);
 	});
 
 	it('should last month', function () {
-		assert.equal(Duration('en')(new Date(NOW), new Date(NOW + Duration.month)), 'last month');
+		check(
+			NOW,
+			NOW + Duration.month,
+			'last month'
+		);
 	});
 
 	it('should next month', function () {
-		assert.equal(Duration('en')(new Date(NOW + Duration.month), new Date(NOW)), 'next month');
+		check(
+			NOW + Duration.month,
+			NOW,
+			'next month'
+		);
 	});
 
 	it('should next year', function () {
-		assert.equal(Duration('en')(new Date(NOW + Duration.year), new Date(NOW)), 'next year');
+		check(
+			NOW + Duration.year,
+			NOW,
+			'next year'
+		);
 	});
 });
